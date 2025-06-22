ff06b8b96473d2d356d4beb652880a776a519b5e
VietHieppp <hiepgale0817@gmail.com>
2025-06-22 22:27:50 +0700

    PDF Reading & QA Feature

--- chat_gemini.py
+++ chat_gemini.py
@@ -1,12 +1,29 @@
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
+import fitz
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel(model_name="models/gemini-pro")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 def chat_with_gemini(prompt):
+    response = model.generate_content(prompt)
+    return response.text
+
+def extract_text_from_pdf(pdf_path):
+    doc=fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+
+    doc.close()
+    return text
+
+def ask_pdf_question(pdf_path, user_question):
+    pdf_text = extract_text_from_pdf(pdf_path)
+    prompt = f"""Below is the content of PDF :\n\n{pdf_text}n\nWith given content, answer these questions:\n{user_question}"""
+
     response = model.generate_content(prompt)
     return response.text
--- main_interface.py
+++ main_interface.py
@@ -1,11 +1,28 @@
 import gradio as gr
-from chat_gemini import chat_with_gemini
+from chat_gemini import chat_with_gemini, ask_pdf_question
 
-iface = gr.Interface(
-    fn = chat_with_gemini,
-    inputs = gr.Textbox(lines=4, placeholder="Input question..."),
-    outputs="text",
-    title="Chat with Gemini",
-)
+def handle_chat(prompt):
+    return chat_with_gemini(prompt)
 
-iface.launch()
+def handle_pdf(pdf, question):
+    if pdf is None or question.strip() == "":
+        return "Vui lòng chọn file PDF và nhập câu hỏi."
+    return ask_pdf_question(pdf.name, question)
+
+with gr.Blocks() as demo:
+    gr.Markdown("## 🤖 Free GEMINI AI ASSISTANT")
+    
+    with gr.Tab("💬 Chat with Gemini (Flash)"):
+        prompt = gr.Textbox(label="Prompt")
+        output = gr.Textbox(label="Response")
+        submit = gr.Button("Send")
+        submit.click(handle_chat, inputs=prompt, outputs=output)
+
+    with gr.Tab("📄 Read & Answer PDF"):
+        pdf = gr.File(label="Upload PDF File", file_types=[".pdf"])
+        question = gr.Textbox(label="Questions")
+        answer = gr.Textbox(label="Answer")
+        ask = gr.Button("Ask")
+        ask.click(handle_pdf, inputs=[pdf, question], outputs=answer)
+
+demo.launch()
